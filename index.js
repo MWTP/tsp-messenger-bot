aee3296db3427e174f86157cde88dc7a53b22c8f
MWTP <nicholaswgiannini@gmail.com>
2020-09-17 15:18:05 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+    if (command === 'embed') {
+		const embed = new Discord.MessageEmbed()
+		.setColor(0x79E621)
+		// .setTitle('Title')
+		.setDescription(args.join(" "))
+		.setAuthor('TSP Messenger', 'https://i.ibb.co/K9NSjHf/speech-balloon-green-m-icon.png')
+		.setThumbnail('https://i.ibb.co/K9NSjHf/speech-balloon-green-m-icon.png')
+        message.channel.send(embed);
+	}
+});
+
+// https://i.ibb.co/NSqWPZ1/tspyellow2stroke.png
+
+client.login(token);
